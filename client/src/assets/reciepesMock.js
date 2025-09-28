95962ca04a9458c5e5ea41220b3ce52c8e66ec43
DomsGlobal9 <domsgloballlp@gmail.com>
2025-09-28 19:45:23 +0530

    hari changes

--- client/src/assets/reciepesMock.js
+++ client/src/assets/reciepesMock.js
@@ -1,28 +1,30 @@
 /* replace this with your fetch later */
-import recipe from "../assets/recipes.png";
+import pancake from "../assets/Pan Cake1.jpg";
+import dosamix from "../assets/Instant Dosa Mix.jpg";4
+import upmamix from "../assets/Upma1.jpg"
 
 // recipesMock.js
 const recipes = [
   {
     id: 1,
-    title: "Crispy Multi Millet Dosa",
-    image: recipe, // Adjust path as needed
+    title: "1 Cup Pancake Mix",
+    image: pancake, // Adjust path as needed
     time: "10 mins",
     tags: ["Breakfast", "Sweet", "Quick", "Pancake"],
     description: "A delicious and healthy dosa made with multi-millet flour.",
   },
    {
     id: 2,
-    title: "Crispy Multi Millet Dosa2",
-     image: recipe, // Adjust path as needed
+    title: "1 Cup Dosa Mix",
+     image: dosamix, // Adjust path as needed
     time: "12 mins",
     tags: ["Breakfast", "Sweet", "Quick", "Pancake"],
     description: "A delicious and healthy dosa made with multi-millet flour.",
   },
    {
     id: 3,
-    title: "Crispy Multi Millet Dosa3",
-     image: recipe, // Adjust path as needed
+    title: "1 Cup Upma Mix",
+     image: upmamix, // Adjust path as needed
     time: "8 mins",
     tags: ["Breakfast", "Sweet", "Quick", "Pancake"],
     description: "A delicious and healthy dosa made with multi-millet flour.",

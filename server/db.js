6f2f1ffda093c7ee1d272a587ab49b4365f4236b
DomsGlobal9 <domsgloballlp@gmail.com>
2026-01-19 17:29:11 +0530

    It pleasure to work with you all and Thank you for the opportunity

--- server/db.js
+++ server/db.js
@@ -6,8 +6,12 @@ dotenv.config();
 
 const { Pool } = pg;
 
+
+
+
 const pool = new Pool({
-  user:     process.env.DB_USER,
+  user:     process.env.DB_USER, 
+  
   host:     process.env.DB_HOST,
   database: process.env.DB_DATABASE,
   password: process.env.DB_PASSWORD,
